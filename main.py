46df6b067c67fae16f9c0ef8f087bdceeb0b54da
Me <park.pavel04@gmail.com>
2023-02-17 15:25:32 +0600

    Update Function

--- main.py
+++ main.py
@@ -19,6 +19,18 @@ class CarShowRoom:
     def add(self, name, model, power, price):
         car = Car(name, price, model, power)
         self.cars.append(car)
-        print(f"{name} Added")
+        print(f"{name} 𝗗𝗢𝗡𝗘 ✔")
 
 
+    #Update Function
+    def Update(self , index, name=None, price=None, model=None, power=None):
+        car = self.cars[index - 1]
+        if name:
+            car.name = name
+        if model:
+            car.model = model
+        if power:
+            car.power = power
+        if price:
+            car.price = price
+        print(f"{car.name} ~𝐔𝐃𝐀𝐓𝐄𝐃~ ")
